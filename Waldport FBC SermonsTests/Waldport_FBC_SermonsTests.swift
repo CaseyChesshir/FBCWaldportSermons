b79a4587fbee9d6679dffeeb636657d7e9d68e11
CaseyChesshir <chesshir@uoregon.edu>
2017-05-14 11:53:13 -0700

    Initial Commit

---
+++ Waldport FBC SermonsTests/Waldport_FBC_SermonsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Waldport_FBC_SermonsTests.swift
+//  Waldport FBC SermonsTests
+//
+//  Created by Casey Chesshir on 5/14/17.
+//  Copyright © 2017 Waldport FBC. All rights reserved.
+//
+
+import XCTest
+@testable import Waldport_FBC_Sermons
+
+class Waldport_FBC_SermonsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
